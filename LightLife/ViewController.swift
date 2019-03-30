cd6f3e76d2b031de2ba79b4fc7ae9f7786f705c9
joan <joanbarrosogarrido@gmail.com>
2019-03-30 16:47:49 +0100

    big bang

---
+++ LightLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightLife
+//
+//  Created by joan barroso on 30/03/2019.
+//  Copyright © 2019 joan barroso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
