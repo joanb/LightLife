9a869fd3a509643830651fe213db77c662d285db
joan <joanbarrosogarrido@gmail.com>
2019-03-31 08:39:25 +0200

    add first buttons and navigation

--- LightLife/ViewController.swift
+++ LightLife/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func onRedLightPressed(_ sender: Any) {
+    }
+    @IBAction func onAmbarLightPressed(_ sender: Any) {
+    }
+    @IBAction func onGreenLightPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
