fbdd3bd3a085953e997d7c70b69d6b45f848ae30
joan <joanbarrosogarrido@gmail.com>
2019-04-03 21:14:12 +0200

    WIP: collectionView fixes

--- LightLife/Reminders/RemindListViewController.swift
+++ LightLife/Reminders/RemindListViewController.swift
@@ -1,19 +1,20 @@
 //
-//  RemindListViewController.swift
+//  CollectionViewController.swift
 //  LightLife
 //
-//  Created by joan barroso on 31/03/2019.
+//  Created by joan barroso on 03/04/2019.
 //  Copyright © 2019 joan barroso. All rights reserved.
 //
 
 import UIKit
 
-class RemindListViewController: UICollectionViewController {
+private let reuseIdentifier = "Cell"
 
-    private let reuseIdentifier = "ReminderCell"
+class RemindListViewController: UICollectionViewController {
+    
     let reminders : [Reminder] = [Reminder(), Reminder()]
     var origin: String?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("came from \(String(describing: origin))")
@@ -22,11 +23,12 @@ class RemindListViewController: UICollectionViewController {
 
 extension RemindListViewController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return reminders.count
+        return 1
     }
     
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return reminders.count
+        return 2
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
